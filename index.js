e0b3f7075c8480e8fa66ab61e9da47072a595435
Harrison-Mordecai <airhorns95@gmail.com>
2022-04-29 11:34:14 +1200

    create calculations

---
+++ index.js
@@ -0,0 +1,16 @@
+// let lightsEl = document.getElementById('n1').value
+// let timeEl = document.getElementById('n2').value
+let resultEl = document.getElementById('result')
+// let total = 0
+
+// function calculate() {
+//   let total = lightsEl * timeEl
+//   resultEl.textContent = 'Krissy has wasted: ' + total
+// }
+
+function calculate() {
+  var numOne = document.getElementById('n1').value
+  var numTwo = document.getElementById('n2').value
+  var total = parseInt(numOne) * parseInt(numTwo) * 0.002
+  resultEl.textContent = 'Krissy has wasted $' + total + ' leaving lights on.'
+}
